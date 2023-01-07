c65cbbfb671836d32f0d7f605970f252e42bf8ab
kiu_kituashvili.zurab <kituashvili.zurab@kiu.edu.ge>
2023-01-07 23:32:26 +0400

    s

--- src/fop/w5cars/Car.java
+++ src/fop/w5cars/Car.java
@@ -4,11 +4,17 @@ public class Car {
     private String brand;
     private LicensePlate licensePlate;
     private int chassisNumber;
-    private static int count = 0;
+    private static int count = 1;
 
     public Car(String brand, LicensePlate licensePlate) {
         this.brand = brand;
         this.licensePlate = licensePlate;
         this.chassisNumber = count++;
+    }
+    public int getChassisNumber(){
+        return this.chassisNumber;
+    }
+    public void setChassisNumber(int newNumber){
+        this.chassisNumber = newNumber;
     }
 }
