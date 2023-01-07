d034ed320c189dbf65c56332b62082e59d812a82
kiu_kituashvili.zurab <kituashvili.zurab@kiu.edu.ge>
2023-01-08 00:15:14 +0400

    s

--- src/fop/w5cars/CarPark.java
+++ src/fop/w5cars/CarPark.java
@@ -3,11 +3,6 @@ package fop.w5cars;
 public class CarPark {
 
     private Car[] spaces;
-//    private Car car;
-//
-//    public Car getCar(){
-//        return this.car;
-//    }
 
     public CarPark(int n) {
         spaces = new Car[n];
@@ -33,7 +28,7 @@ public class CarPark {
 
     int search(LicensePlate lp){
         for (int i = 0; i < spaces.length; i++){
-            if (spaces[i].getLicensePlate() == lp){
+            if (spaces[i] != null && spaces[i].getLicensePlate() == lp){
                 return i;
             }
         }
