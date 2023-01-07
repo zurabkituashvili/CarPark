dcdbb56060f4c50be8a4b23307ae36382264475e
kiu_kituashvili.zurab <kituashvili.zurab@kiu.edu.ge>
2023-01-07 04:27:34 +0400

    s

--- src/fop/w5cars/Car.java
+++ src/fop/w5cars/Car.java
@@ -2,11 +2,12 @@ package fop.w5cars;
 
 public class Car {
     private String brand;
-    LicensePlate licensePlate;
+    private LicensePlate licensePlate;
     private int chassisNumber;
 
-    public Car(String brand) {
+    public Car(String brand, LicensePlate licensePlate) {
         this.brand = brand;
+        this.licensePlate = licensePlate;
         this.chassisNumber+=1;
     }
 }
