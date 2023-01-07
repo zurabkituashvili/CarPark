d67f505b65b122c1ed2aec6d6c6b57e9f5688671
kiu_kituashvili.zurab <kituashvili.zurab@kiu.edu.ge>
2023-01-07 04:59:02 +0400

    s

--- src/fop/w5cars/CarPark.java
+++ src/fop/w5cars/CarPark.java
@@ -14,7 +14,6 @@ public class CarPark {
                 spaces[i] = c;
                 return i;
             }
-            if (spaces[spaces.length - 1] != null) return -1;
         }
         return -1;
     }
