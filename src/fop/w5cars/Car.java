df173a828dcc722da3e65d1d9be994154fb658aa
kiu_kituashvili.zurab <kituashvili.zurab@kiu.edu.ge>
2023-01-07 04:19:50 +0400

    s

--- src/fop/w5cars/Car.java
+++ src/fop/w5cars/Car.java
@@ -1,5 +1,12 @@
 package fop.w5cars;
 
 public class Car {
-	// TODO
+    private String brand;
+    LicensePlate licensePlate;
+    private int chassisNumber;
+
+    public Car(String brand) {
+        this.brand = brand;
+        this.chassisNumber+=1;
+    }
 }
--- src/fop/w5cars/LicensePlate.java
+++ src/fop/w5cars/LicensePlate.java
@@ -1,5 +1,14 @@
 package fop.w5cars;
 
 public class LicensePlate {
-	// TODO
+
+    private String regionalCode, letters;
+    private int digits;
+
+
+    public LicensePlate(String regionalCode, String letters, int digits) {
+        this.regionalCode = regionalCode;
+        this.letters = letters;
+        this.digits = digits;
+    }
 }
